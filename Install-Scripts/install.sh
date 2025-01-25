782cf4350b69f5f8618fe40f1b78f7dfabc0fadc
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-01-26 00:06:13 +0100

    first try's of adding the config

--- Install-Scripts/install.sh
+++ Install-Scripts/install.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
+
+# Handle the User Installations
 sudo apt install zsh eza thefuck
 curl -fsSL https://bun.sh/install | bash
 chmod +x ./Install-Scripts/install-oh-my-zsh.sh
 /bin/bash ./Install-Scripts/install-oh-my-zsh.sh
+
+# Start Configuring the Stuff!
+source $USER/.zshrc
