d88a183e3003d7aa5c741002d553fbe89b5456b8
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-01-26 00:22:23 +0100

    added checkpoints as a form of debugging

--- Install-Scripts/install.sh
+++ Install-Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Handle the User Installations
-sudo apt install zsh eza thefuck
+sudo apt install zsh eza thefuck batcat
 curl -fsSL https://bun.sh/install | bash
 chmod +x ./Install-Scripts/install-oh-my-zsh.sh
 /bin/bash ./Install-Scripts/install-oh-my-zsh.sh
@@ -9,3 +9,5 @@ chmod +x ./Install-Scripts/install-oh-my-zsh.sh
 # Start Configuring the Stuff!
 chmod +x ./Install-Scripts/mvdata.sh
 /bin/bash ./Install-Scripts/mvdata.sh
+
+cat $HOME/.zshrc | grep "alias"
