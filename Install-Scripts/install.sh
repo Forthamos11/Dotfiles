aee8f211f1ff875688884118a53cb04aba5db49c
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-01-25 23:53:37 +0100

    another try at fixing the execution of the installing-oh-my-zsh.sh script

--- Install-Scripts/install.sh
+++ Install-Scripts/install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 sudo apt install zsh eza thefuck
 curl -fsSL https://bun.sh/install | bash
-chmod +x ./install-oh-my-zsh.sh
-/bin/bash ./install-oh-my-zsh.sh
+chmod +x ./Install-Scripts/install-oh-my-zsh.sh
+/bin/bash ./Install-Scripts/install-oh-my-zsh.sh
